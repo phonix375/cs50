6b0ad5fe17c11ffc4ea7e71137c347458e24b721
Alexy Kotliar <54607369+phonix375@users.noreply.github.com>
2019-10-22 13:30:42 -0400

    Update import.py

--- import.py
+++ import.py
@@ -5,7 +5,7 @@ from flask import Flask, render_template, request, session, redirect, url_for
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 app = Flask(__name__)
-engine = create_engine("postgres://lgfawvxjeajxee:e75711224c5ab06c3656a5506cd35917a4c0216c324d735c3c45aaca1bd8b05f@ec2-174-129-241-14.compute-1.amazonaws.com:5432/d34u0l6frujlo0")
+engine = create_engine(os.getenv("DATABASE_URL"),pool_size=20, max_overflow=100)
 db = scoped_session(sessionmaker(bind=engine))
 
 
