1fcdc9374ce8671942f54836e449fef1eda4963b
phonix375 <phonix375@users.noreply.github.com>
2020-05-26 23:07:20 +0000

    automated commit by submit50

--- hello.c
+++ hello.c
@@ -3,6 +3,8 @@
 
 int main(void)
 {
+    //this asks for the usert name
     string name = get_string("What is your name?\n");
+    //this is printing to the screen Hello and the user name
     printf("hello, %s\n", name);
 }
