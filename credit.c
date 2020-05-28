35a96c06e8aa69b62c6cda5068f6505960e15c50
phonix375 <phonix375@users.noreply.github.com>
2020-05-28 03:02:01 +0000

    automated commit by submit50

--- credit.c
+++ credit.c
@@ -3,37 +3,41 @@
 
 int main(void)
 {
+
     long number;
+    //get input from user
     do
     {
         number = get_long("Number:");
     }
-    while(number < 0);
+    while (number < 0);//check if the input is valid
+    //create veriabels
     long i;
     int digits = 1;
     int sumeven = 0;
-    int sumodd =0;
+    int sumodd = 0;
     long tempInt;
-
+//checks the number of digits and counts the odds and even numbers
     for (i = 10; number / i >= 1 ; i *= 10)
     {
-
-        if(digits % 2 == 0)
+        //check if the number is even
+        if (digits % 2 == 0)
         {
-            tempInt = ((number % i) / (i/10)) * 2;
-            sumeven += (tempInt % 10) +  (tempInt - (tempInt % 10)) / 10;
+            tempInt = ((number % i) / (i / 10)) * 2;
+            sumeven += (tempInt % 10) + (tempInt - (tempInt % 10)) / 10;
         }
+        //check if the number is even
         else
         {
-            sumodd += (number % i) / (i/10);
+            sumodd += (number % i) / (i / 10);
         }
         digits++;
-        if(number / (i*10) <= 1 )
+        if (number / (i * 10) <= 1)
         {
-            if(digits % 2 == 0)
+            if (digits % 2 == 0)
             {
                 tempInt = (number / i) * 2;
-                sumeven += (tempInt % 10) +  (tempInt - (tempInt % 10)) / 10;
+                sumeven += (tempInt % 10) + (tempInt - (tempInt % 10)) / 10;
             }
             else
             {
@@ -43,16 +47,19 @@ int main(void)
         }
 
     }
+    //cheks if the sum is correct
     if ((sumeven + sumodd) % 10 == 0)
     {
+        //cheks the first digits of the number
         long start = number;
         while (start > 100)
         {
             start /= 10;
         }
+        //cheks if american
         if (start == 34 || start == 37)
         {
-            if(digits == 15)
+            if (digits == 15)
             {
                 printf("AMEX\n");
             }
@@ -61,25 +68,29 @@ int main(void)
                 printf("INVALID\n");
             }
         }
-        else if (start >50 && start < 56)
+        //cheks if mastercard
+        else if (start > 50 && start < 56)
         {
-            if(digits == 16)
+            if (digits == 16)
             {
                 printf("MASTERCARD\n");
             }
             else
             {
-               printf("INVALID\n");
+                //prints if the card if not valid
+                printf("INVALID\n");
             }
         }
         else
         {
-            if(4 == (start - (start % 10)) / 10 && digits >= 13 && digits <= 16 )
+            //cheks if VISA
+            if (4 == (start - (start % 10)) / 10 && digits >= 13 && digits <= 16)
             {
                 printf("VISA\n");
             }
             else
             {
+                //prints if the card if not valid
                 printf("INVALID\n");
             }
 
@@ -88,6 +99,7 @@ int main(void)
     }
     else
     {
+        //prints if the card if not valid
         printf("INVALID\n");
     }
 }
