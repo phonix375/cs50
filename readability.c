66995cab3f7ebf6ebd554ac4d7f950902af0be19
phonix375 <phonix375@users.noreply.github.com>
2020-05-30 20:36:22 +0000

    automated commit by check50 [check50=True]

--- readability.c
+++ readability.c
@@ -5,28 +5,34 @@
 int main(void)
 {
     string text = get_string("TEXT: ");
+    //createing the veriables
     int len = strlen(text);
     int wordCounter = 0;
-    int latterCounter =0;
-    int sentenceCounter =0;
+    int latterCounter = 0;
+    int sentenceCounter = 0;
     int i;
-    if(len >= 0)
+    //cheking to see if the string is empty
+    if (len >= 0)
     {
-            for (i= 0; i<=len-1;i++ )
+        //looping over all the characters in the string to count word, sentences and latters
+        for (i = 0; i <= len - 1; i++)
         {
-            if(text[i]==' ')
+            //cheking for words
+            if (text[i] == ' ')
             {
-                if (i != 0 && text[i-1] != ' ')
+                //making sure the privius one was not a space as well
+                if (i != 0 && text[i - 1] != ' ')
                 {
                     wordCounter ++;
                 }
 
             }
-            if( ((int)text[i] >= 65 && (int)text[i] <= 90) || ((int)text[i] >= 97 && (int)text[i] <= 122) )
+            //makeing sure with ascii table the curent chare is a latter and not a simbel
+            if (((int)text[i] >= 65 && (int)text[i] <= 90) || ((int)text[i] >= 97 && (int)text[i] <= 122))
             {
-               latterCounter ++;
+                latterCounter ++;
             }
-            if (  text[i] == '.' || text[i] == '!' || text[i] == '?')
+            if (text[i] == '.' || text[i] == '!' || text[i] == '?')
             {
                 sentenceCounter ++;
             }
@@ -36,14 +42,14 @@ int main(void)
     }
     float avarageLet;
     float avarageSen;
-    avarageLet = ((float)latterCounter / (float)wordCounter)*100;
+    avarageLet = ((float)latterCounter / (float)wordCounter) * 100;
     avarageSen = ((float)sentenceCounter / (float)wordCounter) * 100;
-    int Grade =  (int) round((0.0588*avarageLet) - (0.296 * avarageSen) -15.8);
-    if(Grade >= 16)
+    int Grade = (int) round((0.0588 * avarageLet) - (0.296 * avarageSen) - 15.8);
+    if (Grade >= 16)
     {
         printf("Grade 16+\n");
     }
-    else if(Grade < 1 )
+    else if (Grade < 1)
     {
         printf("Before Grade 1\n");
     }
