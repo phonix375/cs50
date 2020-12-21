14410c882c4b4eec96737e75b196cb1920c7c141
phonix375 <phonix375@users.noreply.github.com>
2020-12-21 03:07:31 +0000

    automated commit by submit50

--- mario.py
+++ mario.py
@@ -1,10 +1,10 @@
 import cs50
-#inisialise the hight to a negative number
+# inisialise the hight to a negative number
 hight = -1
 
-#loop to get a valid responce from the user
+# loop to get a valid responce from the user
 while hight < 1 or hight > 8:
     hight = cs50.get_int("Height:")
-#draw the piramid by for loop
+# draw the piramid by for loop
 for i in range(1, hight + 1):
     print(" " * (hight - i) + "#" * i + "  " + "#" * i)
