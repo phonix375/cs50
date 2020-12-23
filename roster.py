0fe2a59ecb5850a336c1edd0029abd24a5829b3b
phonix375 <phonix375@users.noreply.github.com>
2020-12-23 04:39:40 +0000

    automated commit by submit50

--- roster.py
+++ roster.py
@@ -5,23 +5,19 @@ import csv
 # main function
 
 
-def main():
-    stud = []
-    # make sure there is another argument in argV
-    if len(sys.argv) < 2:
-        print("please provide a hous name to show")
-        return 1
-    # open the database
-    db = cs50.SQL("sqlite:///students.db")
-    house = sys.argv[1]
-    res = (db.execute(r"SELECT * FROM students WHERE house = '%s'" % house))
-    for i in res:
-        if i['middle'] != None:
-            print("{} {} {}, born {}".format(i['FIRST'], i['middle'], i['LAST'], i['birth']))
-        else:
-            print("{} {}, born {}".format(i['FIRST'], i['LAST'], i['birth']))
+stud = []
+# make sure there is another argument in argV
+if len(sys.argv) < 2:
+    print("please provide a hous name to show")
+# open the database
+db = cs50.SQL("sqlite:///students.db")
+house = sys.argv[1]
+res = (db.execute(r"SELECT * FROM students WHERE house = '%s' ORDER BY LAST, FIRST" % house))
+for i in res:
+    if i['middle'] != None:
+        print("{} {} {}, born {}".format(i['FIRST'], i['middle'], i['LAST'], i['birth']))
+    else:
+        print("{} {}, born {}".format(i['FIRST'], i['LAST'], i['birth']))
 
 
 # run the main function
-if __name__ == "__main__":
-    main()
